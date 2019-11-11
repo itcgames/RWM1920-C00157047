f0a1358740277d649a8ab747de268cf8618ef7c1
Martin Farrell <C00157047@itcarlow.ie>
2019-11-11 19:36:59 +0000

    First Commit

---
+++ P1-MartinFarrell/Assets/PipeMain.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody>().transform.position.y > this.GetComponent<Transform>().position.y)
+        {
+            Transform otherObject = collision.gameObject.GetComponent<Rigidbody>().transform;
+            otherObject.SetPositionAndRotation(new Vector3(otherObject.position.x, otherObject.position.y - this.GetComponent<Renderer>().bounds.size.y, otherObject.position.z), new Quaternion());
+        }
+        else if (collision.gameObject.GetComponent<Rigidbody>().transform.position.y < this.GetComponent<Transform>().position.y)
+        {
+            Transform otherObject = collision.gameObject.GetComponent<Rigidbody>().transform;
+            otherObject.SetPositionAndRotation(new Vector3(otherObject.position.x, otherObject.position.y + this.GetComponent<Renderer>().bounds.size.y + 1.0f, otherObject.position.z), new Quaternion());
+        }
+
+    }
+}
---
+++ P1-MartinFarrell/Assets/UpwardVelocity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpwardVelocity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 vel = this.gameObject.GetComponent<Rigidbody>().velocity;
+        this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(vel.x, 1.0f, vel.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
